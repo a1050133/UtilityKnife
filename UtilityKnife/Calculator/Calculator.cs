190044e2aa2425873af4c8268b59637c5cfe81b2
a1050133 <a1050133@g-mail.nsysu.edu.tw>
2021-12-22 08:17:03 +0800

    Create Calculator class and method Add

---
+++ UtilityKnife/Calculator/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityKnife.Calculator
+{
+    class Calculator
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
